603d6dffd1efda5ccef3aa4883fbdbc5a1c407d2
hgraphtech <71861726+hgraphtech@users.noreply.github.com>
2021-07-22 21:45:46 +1000

    update styling

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import './App.css';
 import React from 'react';
 import Table from './table.js'
 import Chart from './chart.js'
-
+import Navbar from './navbar.js'
 
 
 function App() {
   return (
+    <>
+    < Navbar />
     <div className="container-fluid">
       <div className='row'>
         < Table />
         < Chart />
       </div>
     </div>
+    </>
   );
 }
 
